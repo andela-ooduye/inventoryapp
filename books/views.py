59b5510edf2a3db38133a15877cea4c7547a55e5
Oduye Oluwayemisi <oduye.oluwayemisi@andela.com>
2016-07-26 15:07:33 +0100

    created the search functionality

--- books/views.py
+++ books/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Book
 
 # Create your views here.
 def index(request):
-    return ""
-    
+    books = Book.objects.order_by('id')[:5]
+    context = {'books' : books}
+    return render(request, 'books/index.html', context)
+
 def results(request):
-    return ""
+    search = request.POST['search']
+    #     skills contain search  OR  title contains search
+    #   query = Q(title=search ) | Q(category=search )
+    books = Book.objects.raw("SELECT * FROM books_book WHERE title LIKE %s OR category LIKE %s", [search, search])
+    context = {'books' : books}
+    return render(request, 'books/index.html', context)
